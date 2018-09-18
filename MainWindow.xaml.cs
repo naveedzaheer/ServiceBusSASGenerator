b6e24a83daf62cb9409558449099594ee60bfd7e
naveedzaheer <naveedzaheer@hotmail.com>
2018-09-18 16:42:53 -0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServiceBusSASGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Code  for generating of SAS token for authorization with Service Bus
+        /// </summary>
+        /// <param name="resourceUri"></param>
+        /// <param name="keyName"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private static string CreateToken(string resourceUri, string keyName, string key, int timeInMinutes)
+        {
+            TimeSpan sinceEpoch = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            var expiry = Convert.ToString((int)sinceEpoch.TotalSeconds + (timeInMinutes*60)); 
+            string stringToSign = HttpUtility.UrlEncode(resourceUri) + "\n" + expiry;
+            HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key));
+
+            var signature = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(stringToSign)));
+            var sasToken = String.Format(CultureInfo.InvariantCulture,
+            "SharedAccessSignature sr={0}&sig={1}&se={2}&skn={3}",
+                HttpUtility.UrlEncode(resourceUri), HttpUtility.UrlEncode(signature), expiry, keyName);
+
+            return sasToken;
+        }
+
+        private void CreateTokenButtonClick(object sender, RoutedEventArgs e)
+        {
+            txtSASToken.Text = CreateToken(txtResource.Text, txtKeyName.Text, txtKeyValue.Text, int.Parse(txtTimeInMinutes.Text));
+        }
+    }
+}
